295a132c5cc22422fdd122ed4baf830264144242
TheMaty <atalay1977@hotmail.com>
2020-07-26 23:34:53 +0100

    Add files via upload

---
+++ ExposeInputEvents/generated/ManifestTypes.d.ts
@@ -0,0 +1,34 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    Controller: ComponentFramework.PropertyTypes.StringProperty;
+    onBlurScript: ComponentFramework.PropertyTypes.StringProperty;
+    onChangeScript: ComponentFramework.PropertyTypes.StringProperty;
+    onCopyScript: ComponentFramework.PropertyTypes.StringProperty;
+    onCutScript: ComponentFramework.PropertyTypes.StringProperty;
+    onClickScript: ComponentFramework.PropertyTypes.StringProperty;
+    ondblClickScript: ComponentFramework.PropertyTypes.StringProperty;
+    onFocusScript: ComponentFramework.PropertyTypes.StringProperty;
+    onFocusInScript: ComponentFramework.PropertyTypes.StringProperty;
+    onFocusOutScript: ComponentFramework.PropertyTypes.StringProperty;
+    onInputScript: ComponentFramework.PropertyTypes.StringProperty;
+    onKeyDownScript: ComponentFramework.PropertyTypes.StringProperty;
+    onKeyUpScript: ComponentFramework.PropertyTypes.StringProperty;
+    onKeyPressScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseDownScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseEnterScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseLeaveScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseMoveScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseOverScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseOutScript: ComponentFramework.PropertyTypes.StringProperty;
+    onMouseUpScript: ComponentFramework.PropertyTypes.StringProperty;
+    onPasteScript: ComponentFramework.PropertyTypes.StringProperty;
+    onSelectScript: ComponentFramework.PropertyTypes.StringProperty;
+    onWheelScript: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    Controller?: string;
+}
